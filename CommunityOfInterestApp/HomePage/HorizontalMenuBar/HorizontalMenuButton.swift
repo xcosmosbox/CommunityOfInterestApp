b6339f25afbeb6736490665db9a45fd5fabc2c7f
San_Check <2162381070@qq.com>
2023-04-10 16:53:32 +1000

    complete the HorizontalMenuButton class

---
+++ CommunityOfInterestApp/HomePage/HorizontalMenuBar/HorizontalMenuButton.swift
@@ -0,0 +1,86 @@
+//
+//  HorizontalMenuButton.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 10/4/2023.
+//
+
+import UIKit
+
+class HorizontalMenuButton: UIButton, ObservableButton {
+
+    
+    var buttonLisenter:ObserverMenu?
+    var selectedState:ButtonState?
+    
+    
+    init(buttonLisenter: ObserverMenu?, title: String) {
+        super.init(frame: .zero)
+        self.buttonLisenter = buttonLisenter
+        setupButton(title:title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setupButton(title:String){
+        // set title
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.black, for: .normal)
+        
+        // set background color
+        self.backgroundColor = .gray
+        
+        // set the button's corner radius
+        self.layer.cornerRadius = CGFloat(10)
+        
+        // set height?
+        
+        // disable button title's line wrapping
+        self.titleLabel?.lineBreakMode = .byWordWrapping
+        self.titleLabel?.numberOfLines = 0
+        self.titleLabel?.sizeToFit()
+        
+        // set button state
+        selectedState = .unselected
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // when user touch the button, the OS will auto-call this method
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        buttonLisenter?.buttonSelected(button: self)
+    }
+    
+    
+    func updateButtonState(state:ButtonState){
+        self.selectedState = state
+        if self.selectedState == .unselected{
+            self.backgroundColor = .gray
+        } else {
+            self.backgroundColor = .blue
+        }
+    }
+    
+    
+    func addObserver(observer: ObserverMenu) {
+        buttonLisenter = observer
+    }
+    
+    func removeObserver(observer: ObserverMenu) {
+        buttonLisenter = nil
+    }
+    
+    
+
+}
