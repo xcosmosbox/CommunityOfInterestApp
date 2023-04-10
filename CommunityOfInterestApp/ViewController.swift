6e96c38c93c55dfe2d15d079886f081d2da2bee3
San_Check <2162381070@qq.com>
2023-04-10 12:09:23 +1000

    Initial Commit

---
+++ CommunityOfInterestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 10/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
