771a0d66331be85f624c32281387695c685bc87a
San_Check <2162381070@qq.com>
2023-04-23 17:20:49 +1000

    create explore Component

---
+++ CommunityOfInterestApp/HomePage/ExploreComponent/CardView.swift
@@ -0,0 +1,115 @@
+//
+//  CardView.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 11/4/2023.
+//
+
+import UIKit
+
+class CardView: UIStackView {
+    
+    let usernameStack = UIStackView()
+    let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 180, height: 30))
+    var like: UIImageView! = nil
+    var postImageView: UIImageView! = nil
+    
+    static let cardHight = CGFloat(250)
+    static let cardWidth = CGFloat(180)
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    func build(username:String, title: String, imagePath: String) -> CardView {
+        
+        // set itself
+        self.axis = .vertical
+        self.alignment = .leading
+        self.spacing = 0
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: CardView.cardWidth),
+            self.heightAnchor.constraint(equalToConstant: CardView.cardHight)
+        ])
+        
+        // set usernameStack
+        usernameStack.axis = .horizontal
+        usernameStack.axis = .horizontal
+        usernameStack.alignment = .trailing
+        usernameStack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            usernameStack.widthAnchor.constraint(equalToConstant: CardView.cardWidth),
+            usernameStack.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        // build username label
+        let name = UILabel()
+        name.text = username
+        name.sizeToFit()
+        
+        // build like symbol
+        // create SymbolConfiguration
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular, scale: .default)
+        // build UIImage
+        let image = UIImage(systemName: "heart", withConfiguration: config)
+        // build UIImageView
+        like = UIImageView(image: image)
+        // setting UIImageView's contentMode
+        like?.contentMode = .scaleAspectFit
+        
+        
+        // build title label
+        titleLabel.text = title
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        titleLabel.numberOfLines = 0
+        
+        
+        // build post image
+        postImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: CardView.cardWidth, height: 200))
+        postImageView?.contentMode = .scaleAspectFill
+        postImageView?.clipsToBounds = true
+        // create the image obj
+        let postImage = UIImage(named: imagePath)
+        // zoom in and out the image
+        let scaledImage = postImage?.scaledImage(toSize: CGSize(width: CardView.cardWidth, height: 200))
+        // set the post image view's image
+        postImageView?.image = scaledImage
+        
+        
+        // build up it self
+        usernameStack.addArrangedSubview(name)
+        usernameStack.addArrangedSubview(like)
+        self.addArrangedSubview(postImageView)
+        self.addArrangedSubview(titleLabel)
+        self.addArrangedSubview(usernameStack)
+        
+        
+        
+        return self
+    }
+    
+    
+    
+}
+
+
+extension UIImage {
+    func scaledImage(toSize newSize: CGSize) -> UIImage? {
+        let aspectRatio = self.size.width / self.size.height
+        let newHeight = newSize.width / aspectRatio
+        let newSize = CGSize(width: newSize.width, height: newHeight)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+}
