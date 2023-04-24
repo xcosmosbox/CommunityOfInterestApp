9b02293e0a23b53cee58f2f8520ecffcb2357600
San_Check <2162381070@qq.com>
2023-04-25 01:24:42 +1000

    create Card and Tag NSObject and create the DatabaseProtocol

---
+++ CommunityOfInterestApp/DataModel/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 25/4/2023.
+//
+
+import UIKit
+
+class Card: NSObject, Codable {
+    var username: String?
+    var title: String?
+    var imagePath: String?
+}
---
+++ CommunityOfInterestApp/DataModel/Tag.swift
@@ -0,0 +1,12 @@
+//
+//  Tag.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 25/4/2023.
+//
+
+import UIKit
+
+class Tag: NSObject, Codable {
+    var name: String?
+}
