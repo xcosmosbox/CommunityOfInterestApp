db7758cb2bc28f135ef7bb9b484c0c09b2500187
San_Check <2162381070@qq.com>
2023-04-30 21:53:24 +1000

    complete Post Detail Page

--- CommunityOfInterestApp/HomePage/DetailPage/DetailViewController.swift
+++ CommunityOfInterestApp/HomePage/DetailPage/DetailViewController.swift
@@ -7,16 +7,78 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, DatabaseListener {
+    var listenerType: ListenerType = .postPage
+    
+
+    
+    
+    
+    
+    @IBOutlet weak var PageScrollController: UIScrollView!
+    
+    
+    @IBOutlet weak var PageContainer: UIView!
+    
+    
+    @IBOutlet weak var PageBarControl: UIPageControl!
+    
+    @IBOutlet weak var TitleTextLabel: UILabel!
+    
+    @IBOutlet weak var ContentTextLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
     
     var card:Card? = nil
+    
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    weak var databaseController: DatabaseProtocol?
+    
 
     override func viewDidLoad() {
+        
+        print("3")
+        
         super.viewDidLoad()
+        
+        print("4")
         
         print("=====")
         print(card)
         print("+++++")
+        
+        
+        databaseController = appDelegate?.databaseController
+        
+        if let viewController = PageContainer.subviews.first?.next as? PageImageViewController{
+            PageBarControl.numberOfPages = viewController.pageNumber
+            PageBarControl.currentPage = 0
+        }
+        
+//        TitleTextLabel.numberOfLines = 0
+        TitleTextLabel.text = card?.title
+//        ContentTextLabel.numberOfLines = 0
+        ContentTextLabel.text = card?.content
+        
+        var counter = 0.0
+        counter += PageContainer.frame.height
+        counter += PageBarControl.frame.height
+        counter += TitleTextLabel.frame.height
+        counter += ContentTextLabel.frame.height
+        
+        
+        PageScrollController.contentSize = CGSize(width: 393, height: counter + 100.0)
+        PageScrollController.showsVerticalScrollIndicator = false
+        PageScrollController.addSubview(PageContainer)
+        PageScrollController.addSubview(PageBarControl)
+        PageScrollController.addSubview(TitleTextLabel)
+        PageScrollController.addSubview(ContentTextLabel)
+        
 
         // Do any additional setup after loading the view.
     }
@@ -31,6 +93,27 @@ class DetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        databaseController?.addListener(listener: self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        databaseController?.removeListener(listener: self)
+    }
+    
+    func onExploreChange(change: DatabaseChange, cards: [Card]) {
+        // nothing to do
+    }
+    
+    func onTagChange(change: DatabaseChange, tags: [Tag]) {
+        // nothing to do
+    }
+    
+    func onImagePageChange(change: DatabaseChange, pageNumber: Int) {
+        self.PageBarControl.currentPage = pageNumber
+    }
 
 }
 
