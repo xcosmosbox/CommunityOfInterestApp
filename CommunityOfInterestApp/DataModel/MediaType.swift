bc6b5c8399c8de231c7f90211befbf67d5bd2c5a
San_Check <2162381070@qq.com>
2023-05-16 19:57:38 +1000

    using Media super class to refactor the PageImageViewController

---
+++ CommunityOfInterestApp/DataModel/MediaType.swift
@@ -0,0 +1,19 @@
+//
+//  MediaType.swift
+//  CommunityOfInterestApp
+//
+//  Created by Yuxiang Feng on 16/5/2023.
+//
+
+import Foundation
+
+
+
+enum MediaType{
+    case image
+    case video
+    case audio
+}
+
+
+
